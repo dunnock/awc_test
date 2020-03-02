4a7e949274f7e2d3fbb3892db3f26398e0aa4f10
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2020-03-02 16:26:18 +0200

    add benchmark

---
+++ benches/getimg.rs
@@ -0,0 +1,72 @@
+// Benchmarking of benefit of readonly pool
+
+use criterion::{Criterion, criterion_main};
+use awc::Client;
+use bytes::Bytes;
+use actix_rt::SystemRunner;
+use reqwest;
+use std::future::Future;
+
+const URL: &'static str = "https://upload.wikimedia.org/wikipedia/commons/f/ff/Pizigani_1367_Chart_10MB.jpg";
+
+fn bench_fn<R>(criterion: &mut Criterion, rt: &mut SystemRunner, f: fn()->R, name: &str)
+where
+	R: Future<Output=Bytes> + 'static,
+{
+	// start benchmark loops
+	criterion.bench_function(name, move |b| {
+		b.iter_custom(|iters| {
+			let elapsed = rt.block_on(async move {
+				let mut total = 0;
+
+				let start = std::time::Instant::now();
+
+				for _ in 0..iters {
+					total += (f)().await.len();
+				}
+		
+				let elapsed = start.elapsed();
+				println!("Throughput {}B/s", total * 1_000_000 / elapsed.as_micros() as usize);
+
+				elapsed
+			});
+			// check that at least first request succeeded
+			elapsed
+		})
+	});
+}
+
+async fn awc_test() -> Bytes {
+	Client::default()
+		.get(URL)
+		.send()
+		.await
+		.expect("awc get send")
+		.body()
+		.await
+		.expect("awc body")
+}
+
+async fn reqwest_test() -> Bytes {
+	reqwest::get(URL)
+		.await
+		.expect("reqwest get")
+		.bytes()
+		.await
+		.expect("reqwest bytes")
+}
+
+pub fn pool_benches() {
+    let mut criterion: ::criterion::Criterion<_> =
+		::criterion::Criterion::default().configure_from_args();
+
+	let mut rt = actix_rt::System::new("test");
+
+	bench_fn(&mut criterion, &mut rt, awc_test, "awc");
+	bench_fn(&mut criterion, &mut rt, reqwest_test, "awc");
+	bench_fn(&mut criterion, &mut rt, awc_test, "awc");
+	bench_fn(&mut criterion, &mut rt, reqwest_test, "awc");
+}
+
+criterion_main!(pool_benches);
+
